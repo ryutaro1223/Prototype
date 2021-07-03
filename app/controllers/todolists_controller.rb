6575c29164dd56bcf86bfb427d090ac0f349a1ba
ryutaro1223 <k.ryutaro1223@gmail.com>
2021-07-03 09:47:43 +0000

    third commit

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,42 @@
 class TodolistsController < ApplicationController
   def new
+    @list = List.new
   end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/'
+  end
+
+  def index
+    @lists = List.all.order("id DESC")
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to '/todolists'
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    list.destroy
+    redirect_to 'todolists'
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
